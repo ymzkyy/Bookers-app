2b6def6ecf0b92aa42ba5201112674a661aed992
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-09-05 02:29:11 +0000

    bookの新規登録まで

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+
+  get 'books' => 'books#index'
+  get ''=> 'homes#top'
+  get  'books' =>'books#new'
+  post 'books' => 'books#create'
+
+  get 'books/show'
+  get 'books/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
