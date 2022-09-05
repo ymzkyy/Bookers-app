7b03b6333f6962846ae746f1fac4e55f334621b6
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-09-05 02:48:19 +0000

    h

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-
   get 'books' => 'books#index'
   get ''=> 'homes#top'
   get  'books' =>'books#new'
@@ -8,5 +6,6 @@ Rails.application.routes.draw do
 
   get 'books/show'
   get 'books/edit'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
