4530b186676daebeb71f1d403bb811f9b46d8844
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-09-06 15:06:50 +0000

    １７

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
@@ -10,4 +17,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get  'books' =>'books#new'
   post 'books' => 'books#create'
 
-  get 'books/show'
+  get 'books/show' => 'books#show'
   get 'books/edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
