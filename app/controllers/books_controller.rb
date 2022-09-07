4aefb4652f36bd60618f9d6fd367d5c661f32ef8
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-09-07 15:20:21 +0000

    課題提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,13 @@ class BooksController < ApplicationController
     
   end
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id) 
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end  
   end
   
 
@@ -24,9 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id) 
+    else
+     @books = Book.all
+      render :show
+    end 
   end
   
   def destroy
